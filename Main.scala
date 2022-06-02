6385c203ef656b5d90842c11010071dd1a7ea4de
Yusuke Yasuoka <yusuke.yasuoka@nextbeat.net>
2022-06-02 19:00:08 +0900

    create map-flatMap method

--- Main.scala
+++ Main.scala
@@ -3,22 +3,23 @@ import com.yusuke.myconcurrent.MyFuture
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val success: MyFuture[Int] = MyFuture.successful(1)
-    success.onComplete(println)
-    success.onComplete {
-      case Success(i) => println(i * 5)
-      case Failure(e) => println(e.getMessage())
-    }
+    // val success: MyFuture[Int] = MyFuture.successful(1)
+    // success.onComplete(println)
+    // success.onComplete {
+    //   case Success(i) => println(i * 5)
+    //   case Failure(e) => println(e.getMessage())
+    // }
 
-    val failed: MyFuture[Int] = MyFuture.failed(new Exception("error!"))
-    failed.onComplete(println)
-    failed.onComplete{
-      case Success(i) => println(i * 5)
-      case Failure(e) => println(e.getMessage())
-    }
+    // val failed: MyFuture[Int] = MyFuture.failed(new Exception("error!"))
+    // failed.onComplete(println)
+    // failed.onComplete{
+    //   case Success(i) => println(i * 5)
+    //   case Failure(e) => println(e.getMessage())
+    // }
 
     // tranforsm, transformWithメソッド作成後
-    MyFuture.successful(3).transform(_ map (_ * 3)).onComplete(println)
-    MyFuture.successful(4).transformWith(i => MyFuture.successful(i.map(_ * 3))).onComplete(println)
+    // MyFuture.successful(3).transform(_ map (_ * 3)).onComplete(println)
+    // MyFuture.successful(4).transformWith(i => MyFuture.successful(i.map(_ * 3))).onComplete(println)
+      
   }
 }
