54519c498f0fb19ea8ea7242c19ba7b232e59361
Yusuke Yasuoka <yusuke.yasuoka@nextbeat.net>
2022-06-03 15:44:22 +0900

    add DefaultFuture

---
+++ DefaultFuture.scala
@@ -0,0 +1,13 @@
+import scala.concurrent._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.util.{Try, Success, Failure}
+
+object DefaultFuture{
+  Future.successful(123).onComplete(println)
+
+  val future1 = Future.successful(123).transform(_ map (_ * 2))
+  val future2 = Future.successful(123).transformWith(i => MyFuture.successful(i.map(_ * 2)))
+
+  Future.successful(1).flatMap(i => Future.successful(2).map(_ + i)).onComplete(println)
+}
