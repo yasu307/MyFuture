90583513fb4bf63587edc0f6e3e1df60899d6f42
Yusuke Yasuoka <yusuke.yasuoka@nextbeat.net>
2022-06-05 21:46:38 +0900

    add comment about bug

--- Main.scala
+++ Main.scala
@@ -19,6 +19,11 @@ object Main {
     // tranforsm, transformWithメソッド作成後
     // MyFuture.successful(3).transform(_ map (_ * 3))
     // MyFuture.successful(4).transformWith(i => MyFuture.successful(i.map(_ * 3)))
+
+    // 稀に結果が表示されないバグがあります　詳しい発生原因は分かりません
+    // 結果が表示される時もあるので別スレッドの処理が終わる前にメインスレッドが終了してしまっていると思っていたのですが
+    // Thread.sleepしても改善されないため何かしら別の原因があるのかもしれません。
+    // 原因を突き止めようと内部処理の中でprintやloggerを使用すると、結果が表示されてしまうため、原因がわからない状態です
     MyFuture.successful(1).flatMap(i => MyFuture.successful(2).map(_ + i)).onComplete(println)
   }
 }
